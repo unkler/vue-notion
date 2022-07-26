18ea4af12fcb9949d89c7b29fc64797a37e15a85
unkler <poru@add-natural.com>
2022-07-26 21:07:47 +0900

    ベース部分の実装及び不要なファイルを削除

---
+++ src/components/MainPage.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="main-page">
+    <div class="left-menu" @click.self="onEditNoteEnd()">
+      <!-- ノートリスト -->
+      <note-item
+        v-for="note in noteList"
+        :note="note"
+        :key="note.id"
+        @delete="onDeleteNote"
+        @editStart="onEditNoteStart"
+        @editEnd="onEditNoteEnd"
+      />
+
+      <!-- ノート追加ボタン -->
+      <button class="transparent" @click="onClickButtonAdd">
+        <i class="fas fa-plus-square"></i>ノートを追加
+      </button>
+    </div>
+    <div class="right-view" @click.self="onEditNoteEnd()">
+      右ビュー
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import NoteItem from '@/components/parts/NoteItem.vue'
+
+export default {
+  components: {
+    NoteItem,
+  },
+  data() {
+    return {
+      noteList: [],
+    }
+  },
+  methods: {
+    onClickButtonAdd() {
+      this.noteList.push({
+        id: new Date().getTime().toString(16),
+        name: `新規ノート`,
+        mouseover: false,
+        editing: false,
+      })
+    },
+    onDeleteNote(deleteNote) {
+      const index = this.noteList.indexOf(deleteNote)
+      this.noteList.splice(index, 1)
+    },
+    onEditNoteStart(editNote) {
+      for (let note of this.noteList) {
+        note.editing = (note.id === editNote.id)
+      }
+    },
+    onEditNoteEnd() {
+      for (let note of this.noteList) {
+        note.editing = false
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.main-page {
+  display: flex;
+  height: calc(100vh - 60px);
+  .left-menu {
+    width: 350px;
+    background-color: #f7f6f3;
+  }
+  .right-view {
+    flex-grow: 1;
+    padding: 10px;
+  }
+}
+</style>
---
+++ src/components/study/DirectivePage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="directive-page">
+    <h2>学習2：ディレクティブ</h2>
+
+    <!-- 従来のJavaScript -->
+    <h3><span class="badge badge-secondary">JavaScript</span></h3>
+    <input id="check" type="checkbox" checked="checked">
+    <span id="text">チェック時だけ見えるよ</span>
+    <br />
+    <br />
+
+    <!-- Vue.js -->
+    <h3><span class="badge badge-secondary">Vue.js</span></h3>
+    <input type="checkbox" v-model="checked">
+    <span v-show="checked">チェック時だけ見えるよ</span>
+  </div>
+</template>
+
+<script>
+// 従来のJavaScript
+window.onload = () => {
+  const checkbox = document.getElementById('check');
+  checkbox.onchange = (event) => {
+    const checked = event.target.checked;
+    const text = document.getElementById('text');
+    if (checked) {
+      text.style.visibility = 'visible';
+      return;
+    }
+    text.style.visibility = 'hidden';
+  }
+}
+
+export default {
+  data() {
+    return {
+      checked: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.directive-page {
+  padding: 20px;
+}
+</style>
